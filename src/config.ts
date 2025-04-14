2b763c87cda24be0cdee55bfb43dc350c964e0d8
idanke <idanke.zm@gmail.com>
2025-04-14 14:09:21 +0800

    Update and rename config.ts.template to config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@ import { TOKEN_PROGRAM_ID, TOKEN_2022_PROGRAM_ID } from '@solana/spl-token'
 import bs58 from 'bs58'
 
 export const owner: Keypair = Keypair.fromSecretKey(bs58.decode('<YOUR_WALLET_SECRET_KEY>'))
-export const connection = new Connection('<YOUR_RPC_URL>') //<YOUR_RPC_URL>
-// export const connection = new Connection(clusterApiUrl('devnet')) //<YOUR_RPC_URL>
+// export const connection = new Connection('<YOUR_RPC_URL>') //<YOUR_RPC_URL>
+export const connection = new Connection(clusterApiUrl('devnet')) //<YOUR_RPC_URL>
 export const txVersion = TxVersion.V0 // or TxVersion.LEGACY
 const cluster = 'mainnet' // 'mainnet' | 'devnet'
 
